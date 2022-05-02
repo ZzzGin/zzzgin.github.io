0a807e622e2bbce6bac5a5dbbbed3e7da9b8a3f5
ZzzGin <13491630+ZzzGin@users.noreply.github.com>
2022-05-01 18:16:57 -0700

    restructured the folder for the first artwork

---
+++ src/pages/artworks/aperture-laboratries/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Logo1Animation from "./assets/images/Logo-1-animation.svg"
+
+// markup
+const ApertureLaboratries = () => {
+  return (
+    <>
+      <img src={ Logo1Animation } alt="logo-1-animation" />
+    </>
+  )
+}
+
+export default ApertureLaboratries
