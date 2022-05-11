badd5977b1f6e67774ae46e8f840e2f38849bee9
ZzzGin <13491630+ZzzGin@users.noreply.github.com>
2022-05-10 21:31:09 -0700

    create zzz logo

---
+++ src/pages/artworks/logo-design/zzz-logo/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ZzzLogoAnimation from "@components/artworks/logo-design/zzz-logo/svg-components/zzz_logo_animation";
+
+const ZzzLogo = () => {
+
+  return (
+    <>
+        <ZzzLogoAnimation />
+    </>
+  )
+}
+
+export default ZzzLogo;
