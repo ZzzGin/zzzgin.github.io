c6d00ed4eb39e686a7d544bb59705fa0f797dbd5
ZzzGin <13491630+ZzzGin@users.noreply.github.com>
2022-06-01 23:07:01 -0700

    Create a test page for Vis.js

--- src/pages/artworks/timeline/test-vis-js/index.js
+++ src/pages/artworks/timeline/test-vis-js/index.js
@@ -1,2 +1,14 @@
+import * as React from "react"
 import "./test-vis-js.css"
 
+const testVisJs = () => {
+    return (
+        <>
+        <div>
+
+        </div>
+        </>
+    );
+}
+
+export default testVisJs;
